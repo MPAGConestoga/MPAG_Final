06d6718b15ad9067a49aa9a5879653f6461f89c8
Gabriel Gurgel <Ggurgel8686@conestogac.on.ca>
2019-12-08 22:11:09 -0500

    :help: Help me mike

--- MPAG_Final/Planner/ViewModels/OrderViewModel.cs
+++ MPAG_Final/Planner/ViewModels/OrderViewModel.cs
@@ -20,25 +20,10 @@ namespace MPAG_Final.Planner.ViewModels
    */
     public class OrderViewModel : ObservableObject
     {
-        //PlannerRole planner = new PlannerRole("Planner", "Test", "plannerTest@gmail.com",
-        //    "9009009000", "DamnStreet", "Toronto", "Ontario", "N2E0E2");
-        //public object CurrentView
-        //{
-        //    get { return _currentView; }
-        //    set { OnPropertyChanged(ref _currentView, value); }
-        //}
-
-        private ICarrierDataService _carrierService;
-        private IContractDataService _contractService;
-
-        // for accessing the contracts view model
-        private ContractsViewModel _contractsVM;
-        public ContractsViewModel ContractsVM
-        {
-            get { return _contractsVM; }
-            set { OnPropertyChanged(ref _contractsVM, value); }
-        }
-        // for accessing the contracts view model_plannerRole
+        // Current selected order information
+        private Order FirstOrderSelected = null;
+        public ICommand CheckOrderCommand { get; private set; }
+        // For accessing planner methods 
         private PlannerRole _plannerRoleVM;
         public PlannerRole PlannerRoleVM
         {
@@ -46,14 +31,70 @@ namespace MPAG_Final.Planner.ViewModels
             set { OnPropertyChanged(ref _plannerRoleVM, value); }
         }
 
-        private CarriersViewModel _carriersVM;
-        public CarriersViewModel CarriersVM
+        //-> Order's lists
+        private ObservableCollection<Order> _ftlOrders;
+        public ObservableCollection<Order> FTLOrders
         {
-            get { return _carriersVM; }
-            set { OnPropertyChanged(ref _carriersVM, value); }
+            get { return _ftlOrders; }
+            set
+            {
+                _ftlOrders = value;
+                OnPropertyChanged("FTLOrders");
+
+            }
         }
 
-        public ICommand RunTestCommand { get; private set; }
+        private ObservableCollection<Order> _ltlOrders;
+        public ObservableCollection<Order> LTLOrders
+        {
+            get { return _ltlOrders; }
+            set
+            {
+                _ltlOrders = value;
+                OnPropertyChanged("LTLOrders");
+
+            }
+        }
+
+        //-> Relevant Carriers
+        private ObservableCollection<Carrier> _relevantCarriers;
+        public ObservableCollection<Carrier> RelevantCarriers
+        {
+            get { return _relevantCarriers; }
+            set
+            {
+                _relevantCarriers = value;
+                OnPropertyChanged("RelevantCarriers");
+
+            }
+        }
+
+        //-> Selected Propreties
+        private ObservableCollection<Order> _selectedOrders;
+        public ObservableCollection<Order> SelectedOrders
+        {
+            get { return _selectedOrders; }
+            set
+            {
+                _selectedOrders = value;
+                OnPropertyChanged("SelectedOrders");
+
+            }
+        }
+
+
+        private List<Carrier> _selectedCarriers = new List<Carrier>();
+        public List<Carrier> SelectedCarriers
+        {
+            get { return _selectedCarriers; }
+            set
+            {
+                _selectedCarriers = value;
+                OnPropertyChanged("SelectedCarriers");
+
+            }
+        }
+
 
 
         /// <summary>
@@ -61,39 +102,42 @@ namespace MPAG_Final.Planner.ViewModels
         /// </summary>
         public OrderViewModel(PlannerRole planner)
         {
-            var carrierMarketPlace = new MockCarrierMarketplace(); //mock service for the testing of the ui
-            var contractMarketPlace = new MockContractMarketplace(); //mock service for the testing of the ui
-
             PlannerRoleVM = planner;
 
-            _carrierService = carrierMarketPlace;
-            _contractService = contractMarketPlace;
-
-            RunTestCommand = new RelayCommand(RunTest);
-
-          //  ContractsVM = new ContractsViewModel(contractMarketPlace);
-            CarriersVM = new CarriersViewModel(carrierMarketPlace);
-           // LoadCarriers();
-           // LoadContracts();
-
-            int i = 0;          
+            // DEBUG: Include FTL Order 
+            LTLOrders = new ObservableCollection<Order>(new SampleData().SampleLTLOrders());
+            SelectedOrders = new ObservableCollection<Order>();
+            CheckOrderCommand = new SelectOrder(this);
         }
 
-
-        private void LoadContracts()
+        public void OrderChecked(object parameter)
         {
-            ContractsVM.LoadContracts(_contractService.GetContracts());
-        }
+            var list = (object[])parameter;
+            Order selectedOrder = (Order)list[0];
+            if (FirstOrderSelected == null)
+            {
+                FirstOrderSelected = selectedOrder;
 
-        private void LoadCarriers()
-        {
-            CarriersVM.LoadCarriers(_carrierService.GetCarriers());
-        }
+                // Populate LTL order 
+                LTLOrders = new ObservableCollection<Order>(new SampleData().FilterLTLs(selectedOrder.origin, selectedOrder.vanType));
+                LTLOrders.Remove(selectedOrder);
+                OnPropertyChanged("LTLOrders");
 
+                // Send selected order to bundled orders
+                SelectedOrders.Add(selectedOrder);
+            }
+            else
+            {
+                SelectedOrders.Add(selectedOrder);
+                LTLOrders.Remove(selectedOrder);
+            }
 
-        private void RunTest()
-        {
-            PlannerRoleVM.testFunction();
+            OnPropertyChanged("SelectedOrders");
+
+            // Populate Relevant Carriers list
+            RelevantCarriers = new ObservableCollection<Carrier>(new SampleData().GetRelevantCarrier(selectedOrder.origin, selectedOrder.destination));
+
+            OnPropertyChanged("RelevantCarriers");
         }
 
     }
